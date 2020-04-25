3da939c54a8b493f175ca3678b8cd243f3863ee3
nbutmickey <mickey.nbut@gmail.com>
2020-04-25 21:43:54 +0800

    port changed

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
     production : {
       user : 'root',
       host : '121.199.49.71',
-      port : '20',
+      port : '22',
       ref  : 'origin/master',
       repo : 'https://github.com/nbutmickey/Server_performance_monitoring_platform.git',
       path : '/var/www/production',
