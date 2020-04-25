468a836f906bea5ed8427de3bdd3a4355062468d
nbutmickey <mickey.nbut@gmail.com>
2020-04-25 16:33:31 +0800

    部署配置

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,17 +1,18 @@
 module.exports = {
   apps : [{
-    script: './bin/www',
+    script: './server/bin/www',
   }],
 
   deploy : {
     production : {
       user : 'root',
       host : '121.199.49.71',
+      port : '20',
       ref  : 'origin/master',
       repo : 'https://github.com/nbutmickey/Server_performance_monitoring_platform.git',
       path : '/var/www/production',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
