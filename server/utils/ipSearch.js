0c441472819195472801335c095ddd94d7dfc93a
nbutmickey <mickey.nbut@gmail.com>
2020-05-01 21:36:30 +0800

    deploy

--- server/utils/ipSearch.js
+++ server/utils/ipSearch.js
@@ -1,7 +1,7 @@
 let request=require("request")
 function  getIPInfo(ipaddr) {
     return new Promise((resolve,reject)=>{
-        request.get("http://hubing.online:8085/lookUp?ip="+ipaddr, function(err, response){
+        request.get("http://smallsite:8085/lookUp?ip="+ipaddr, function(err, response){
                let { success,data}=JSON.parse(response.body);
                if(success){
                    resolve({
